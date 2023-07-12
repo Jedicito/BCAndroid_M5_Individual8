b294da3855bdeb7988a304a519cf1e63733c4b6b
Jedicito <seron.cesar@gmail.com>
2023-07-11 21:25:57 -0400

    Se pasa a Kotlin

--- app/src/main/java/chl/ancud/m4_individual4/MainActivity.kt
+++ app/src/main/java/chl/ancud/m4_individual4/MainActivity.kt
@@ -1,102 +1,88 @@
-package chl.ancud.m4_individual4;
+package chl.ancud.m4_individual4
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView cajaMensaje1 = findViewById(R.id.textNumber1);
-        TextView cajaMensaje2 = findViewById(R.id.textNumber2);
-
-        Button botonSumar = findViewById(R.id.botonSumar);
-        Button botonRestar = findViewById(R.id.botonRestar);
-        Button botonDividir = findViewById(R.id.botonDividir);
-        Button botonMultiplicar = findViewById(R.id.botonMultiplicar);
-        Button botonSalir = findViewById(R.id.btn_salir);
-        botonSumar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String numero1 = cajaMensaje1.getText().toString();
-                String numero2 = cajaMensaje2.getText().toString();
-
-
-                if(!numero1.trim().isEmpty() && !numero2.trim().isEmpty()) {
-                    Integer resultado = Integer.valueOf(numero1) + Integer.valueOf(numero2);
-                    Toast.makeText(getBaseContext(), "Sumaste: " + numero1 + "+" + numero2 + " = " + resultado, Toast.LENGTH_LONG).show();
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val cajaMensaje1 = findViewById<TextView>(R.id.textNumber1)
+        val cajaMensaje2 = findViewById<TextView>(R.id.textNumber2)
+        val botonSumar = findViewById<Button>(R.id.botonSumar)
+        val botonRestar = findViewById<Button>(R.id.botonRestar)
+        val botonDividir = findViewById<Button>(R.id.botonDividir)
+        val botonMultiplicar = findViewById<Button>(R.id.botonMultiplicar)
+        val botonSalir = findViewById<Button>(R.id.btn_salir)
+        botonSumar.setOnClickListener {
+            val numero1 = cajaMensaje1.text.toString()
+            val numero2 = cajaMensaje2.text.toString()
+            if (!numero1.trim { it <= ' ' }.isEmpty() && !numero2.trim { it <= ' ' }.isEmpty()) {
+                val resultado = Integer.valueOf(numero1) + Integer.valueOf(numero2)
+                Toast.makeText(
+                    baseContext,
+                    "Sumaste: $numero1+$numero2 = $resultado",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                //Log.d("TAGeo", "onClick Suma else: " + numero1 + "; " + numero2);
+                Toast.makeText(baseContext, "Valores proporcionados no válidos", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        botonRestar.setOnClickListener {
+            val numero1 = cajaMensaje1.text.toString()
+            val numero2 = cajaMensaje2.text.toString()
+            if (!numero1.trim { it <= ' ' }.isEmpty() && !numero2.trim { it <= ' ' }.isEmpty()) {
+                val resultado = Integer.valueOf(numero1) - Integer.valueOf(numero2)
+                Toast.makeText(
+                    baseContext,
+                    "Sumaste: $numero1-$numero2 = $resultado",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(baseContext, "Valores proporcionados no válidos", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        botonMultiplicar.setOnClickListener {
+            val numero1 = cajaMensaje1.text.toString()
+            val numero2 = cajaMensaje2.text.toString()
+            if (!numero1.trim { it <= ' ' }.isEmpty() && !numero2.trim { it <= ' ' }.isEmpty()) {
+                val resultado = Integer.valueOf(numero1) * Integer.valueOf(numero2)
+                Toast.makeText(
+                    baseContext,
+                    "Sumaste: $numero1*$numero2 = $resultado",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(baseContext, "Valores proporcionados no válidos", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        botonDividir.setOnClickListener {
+            val numero1 = cajaMensaje1.text.toString()
+            val numero2 = cajaMensaje2.text.toString()
+            if (!numero1.trim { it <= ' ' }.isEmpty() && !numero2.trim { it <= ' ' }.isEmpty()) {
+                if (java.lang.Float.valueOf(numero2) != 0f) {
+                    val resultado =
+                        java.lang.Float.valueOf(numero1) / java.lang.Float.valueOf(numero2)
+                    Toast.makeText(
+                        baseContext,
+                        "Sumaste: $numero1/$numero2 = $resultado",
+                        Toast.LENGTH_LONG
+                    ).show()
                 } else {
-                    //Log.d("TAGeo", "onClick Suma else: " + numero1 + "; " + numero2);
-                    Toast.makeText(getBaseContext(), "Valores proporcionados no válidos", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(baseContext, "Infinito", Toast.LENGTH_LONG).show()
                 }
+            } else {
+                Toast.makeText(baseContext, "Valores proporcionados no válidos", Toast.LENGTH_SHORT)
+                    .show()
             }
-        });
-
-        botonRestar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String numero1 = cajaMensaje1.getText().toString();
-                String numero2 = cajaMensaje2.getText().toString();
-
-                if(!numero1.trim().isEmpty() && !numero2.trim().isEmpty()) {
-                    Integer resultado = Integer.valueOf(numero1) - Integer.valueOf(numero2);
-                    Toast.makeText(getBaseContext(), "Sumaste: " + numero1 + "-" + numero2 + " = " + resultado, Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getBaseContext(), "Valores proporcionados no válidos", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        botonMultiplicar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String numero1 = cajaMensaje1.getText().toString();
-                String numero2 = cajaMensaje2.getText().toString();
-
-                if(!numero1.trim().isEmpty() && !numero2.trim().isEmpty()) {
-                Integer resultado = Integer.valueOf(numero1) * Integer.valueOf(numero2);
-
-                Toast.makeText(getBaseContext(), "Sumaste: " + numero1 + "*" + numero2 + " = " + resultado, Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getBaseContext(), "Valores proporcionados no válidos", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        botonDividir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String numero1 = cajaMensaje1.getText().toString();
-                String numero2 = cajaMensaje2.getText().toString();
-
-                if(!numero1.trim().isEmpty() && !numero2.trim().isEmpty()) {
-                    if(Float.valueOf(numero2) != 0) {
-                        Float resultado = Float.valueOf(numero1) / Float.valueOf(numero2);
-                        Toast.makeText(getBaseContext(), "Sumaste: " + numero1 + "/" + numero2 + " = " + resultado, Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getBaseContext(), "Infinito", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    Toast.makeText(getBaseContext(), "Valores proporcionados no válidos", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        botonSalir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finishAndRemoveTask();
-            }
-        });
-
+        }
+        botonSalir.setOnClickListener { finishAndRemoveTask() }
     }
 }
